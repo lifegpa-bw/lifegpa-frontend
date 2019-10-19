9656732a2a5658ca8005074db17596cb40e599e9
WStrellis <wstrellis@gmail.com>
2019-10-19 11:51:04 -0400

    added mui

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import logo from './logo.svg'
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <h1>Test!</h1>
+    </div>
+  )
+}
+
+export default App
