cb44af7218c13216c72e0230b26f2920db821dae
WStrellis <wstrellis@gmail.com>
2019-10-22 09:21:04 -0400

    added category field to testData

--- src/testData.js
+++ src/testData.js
@@ -1,5 +1,12 @@
 export const testData = {
   username: '',
+  categories: [
+    { id: 1, description: 'exercise', color: '#10aff4' },
+    { id: 2, description: 'diet', color: '#ff11fa' },
+    { id: 3, description: 'hygiene', color: 'cbcc4a' },
+    { id: 4, description: 'social', color: '#454a11' },
+    { id: 5, description: 'drugs', color: '#bbc184' }
+  ],
   history: [
     {
       date: 10172019,
@@ -7,31 +14,31 @@ export const testData = {
         {
           description: 'stretch for 15 minutes',
           type: 'good',
-          category: { description: 'exercise', color: '#10aff4' },
+          category: 1,
           performed: false
         },
         {
           description: 'ate  a fruit',
           type: 'good',
-          category: { description: 'diet', color: '#ff11fa' },
+          category: 2,
           performed: false
         },
         {
           description: 'brush teeth',
           type: 'good',
-          category: { description: 'hygiene', color: 'cbcc4a' },
+          category: 3,
           performed: false
         },
         {
           description: 'socialize for 1 hour',
           type: 'good',
-          category: { description: 'social', color: '#454a11' },
+          category: 4,
           performed: true
         },
         {
           description: 'Smoke cigarettes',
           type: 'bad',
-          category: { description: 'drugs', color: '#bbc184' },
+          category: 5,
           performed: false
         }
       ]
@@ -42,31 +49,31 @@ export const testData = {
         {
           description: 'stretch for 15 minutes',
           type: 'good',
-          category: { description: 'exercise', color: '#10aff4' },
+          category: 1,
           performed: true
         },
         {
           description: 'ate  a fruit',
           type: 'good',
-          category: { description: 'diet', color: '#ff11fa' },
+          category: 2,
           performed: true
         },
         {
           description: 'brush teeth',
           type: 'good',
-          category: { description: 'hygiene', color: 'cbcc4a' },
+          category: 3,
           performed: false
         },
         {
           description: 'socialize for 1 hour',
           type: 'good',
-          category: { description: 'social', color: '#454a11' },
+          category: 4,
           performed: true
         },
         {
           description: 'Smoke cigarettes',
           type: 'bad',
-          category: { description: 'drugs', color: '#bbc184' },
+          category: 5,
           performed: true
         }
       ]
@@ -77,31 +84,31 @@ export const testData = {
         {
           description: 'stretch for 15 minutes',
           type: 'good',
-          category: { description: 'exercise', color: '#10aff4' },
+          category: 1,
           performed: false
         },
         {
           description: 'ate  a fruit',
           type: 'good',
-          category: { description: 'diet', color: '#ff11fa' },
+          category: 2,
           performed: false
         },
         {
           description: 'brush teeth',
           type: 'good',
-          category: { description: 'hygiene', color: 'cbcc4a' },
+          category: 3,
           performed: false
         },
         {
           description: 'socialize for 1 hour',
           type: 'good',
-          category: { description: 'social', color: '#454a11' },
+          category: 4,
           performed: true
         },
         {
           description: 'Smoke cigarettes',
           type: 'bad',
-          category: { description: 'drugs', color: '#bbc184' },
+          category: 5,
           performed: false
         }
       ]
@@ -112,31 +119,31 @@ export const testData = {
         {
           description: 'stretch for 15 minutes',
           type: 'good',
-          category: { description: 'exercise', color: '#10aff4' },
+          category: 1,
           performed: false
         },
         {
           description: 'ate  a fruit',
           type: 'good',
-          category: { description: 'diet', color: '#ff11fa' },
+          category: 2,
           performed: false
         },
         {
           description: 'brush teeth',
           type: 'good',
-          category: { description: 'hygiene', color: 'cbcc4a' },
+          category: 3,
           performed: false
         },
         {
           description: 'socialize for 1 hour',
           type: 'good',
-          category: { description: 'social', color: '#454a11' },
+          category: 4,
           performed: true
         },
         {
           description: 'Smoke cigarettes',
           type: 'bad',
-          category: { description: 'drugs', color: '#bbc184' },
+          category: 5,
           performed: false
         }
       ]
