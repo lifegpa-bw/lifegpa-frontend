5370b5d6be325b3463343a4d64f3d895975a56c3
WStrellis <wstrellis@gmail.com>
2019-10-23 01:55:54 -0400

    updated forms and created actions for creating/deleting habits

---
+++ src/actions/deleteHabit.js
@@ -0,0 +1,24 @@
+import { axiosWithAuth } from '../utils'
+
+// +++++ DELETE ++++++
+export const dhTypes = {
+  START: 'DH_START',
+  SUCCESS: 'DH_SUCCESS',
+  FAIL: 'DH_FAIL'
+}
+
+export const deleteHabit = id => dispatch => {
+  // set store.isFetching : true
+  dispatch({ type: dhTypes.START })
+  axiosWithAuth()
+    .delete(`http://localhost:3333/api/${id}`)
+    .then(res => {
+      // if the api call is successful trigger the DELETE action
+      // console.log('Delete Response',res)
+      dispatch({ type: dhTypes.SUCCESS, payload: res.data })
+    })
+    .catch(err => {
+      // if the api call fails trigger the FAIL action
+      dispatch({ type: dhTypes.FAIL, payload: err.response })
+    })
+}
--- src/components/HabitCard.jsx
+++ src/components/HabitCard.jsx
@@ -1,41 +1,37 @@
-import React, { useState } from 'react';
-import Habits from '../components/CreateHabit';
+import React, { useState } from 'react'
+import Habits from '../components/CreateHabit'
 import { testData } from '../testData'
 import styled from 'styled-components'
-import { deleteItem } from '../actions/axios_delete_action'
+import { deleteHabit } from '../actions'
 
-const Habit = (props) => {
-  const [remove, setRemove] = useState();
-  
+const Habit = props => {
+  const [remove, setRemove] = useState()
+
   // const removeHabit = e=> {
   //   const id = e.target.getAttribute('id')
   //     setRemove(remove.filter(item => item.id !== id));
   // }
-  
+
   return (
     <Card>
-
-        <p> {props.description} </p>
-        <p> {props.type} </p>
-        <Button>
-          Delete
-        </Button>
-
+      <p> {props.description} </p>
+      <p> {props.type} </p>
+      <Button>Delete</Button>
     </Card>
   )
 }
-export default Habit;
+export default Habit
 
 const Card = styled.div`
-border-radius:10px;
-background:#F5DEB3;
-display:flex;
-justify-content:space-around;
-margin:10px auto;
-text-align:center;
-width:80%;
+  border-radius: 10px;
+  background: #f5deb3;
+  display: flex;
+  justify-content: space-around;
+  margin: 10px auto;
+  text-align: center;
+  width: 80%;
 `
 const Button = styled.button`
-border-radius:5px;
-background:pink;
+  border-radius: 5px;
+  background: pink;
 `
