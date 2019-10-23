48139036cc878f0cc960f060bb3a979e2ab02339
Oluwajoba <oluwajobabello@gmail.com>
2019-10-23 23:31:06 +0100

    create specification components and add specification function / Specification.jsx

--- src/pages/Specifications.jsx
+++ src/pages/Specifications.jsx
@@ -1,29 +1,26 @@
-import React from "react";
-import ReactDOM from "react-dom";
 
-import { CircularProgressbar } from 'react-circular-progressbar';
-import 'react-circular-progressbar/dist/styles.css';
+      import React from "react";
+      import ReactDOM from "react-dom";
+      
+      import { CircularProgressbar } from 'react-circular-progressbar';
+      import 'react-circular-progressbar/dist/styles.css';
+      
+      import "./styles.css";
 
-import "./styles.css";
-
-
-const percentage = 9;
-
-
-
-function Specifications() {
-  return (
-    <div className="App">
-      <h1>Specifications</h1>
-      <CircularProgressbar value={percentage} text={`${percentage}%`} />;
-      <CircularProgressbar value={percentage} text={`${percentage}%`} />;
-      <CircularProgressbar value={percentage} text={`${percentage}%`} />;
-      <CircularProgressbar value={percentage} text={`${percentage}%`} />;
-      <CircularProgressbar value={percentage} text={`${percentage}%`} />;
-      <CircularProgressbar value={percentage} text={`${percentage}%`} />;
-      <CircularProgressbar value={percentage} text={`${percentage}%`} />;
-    </div>
-  );
-}
-
-export default Specifications;
+      function Specifications() {
+        return (
+          <div className="App">
+            <h1>Specifications</h1>
+            <CircularProgressbar value={percentage} text={`${percentage}%`} />;
+            <CircularProgressbar value={percentage} text={`${percentage}%`} />;
+            <CircularProgressbar value={percentage} text={`${percentage}%`} />;
+            <CircularProgressbar value={percentage} text={`${percentage}%`} />;
+            <CircularProgressbar value={percentage} text={`${percentage}%`} />;
+            <CircularProgressbar value={percentage} text={`${percentage}%`} />;
+            <CircularProgressbar value={percentage} text={`${percentage}%`} />;
+          </div>
+        );
+      }
+      
+      export default Specifications;
+      
